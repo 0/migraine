c89e32180974b9cf922a7f98d9a90626d74d2656
Dmitri Iouchtchenko <johnnyspoon@gmail.com>
2012-07-09 23:08:10 -0400

    Made more readable

--- stripes.pl
+++ stripes.pl
@@ -11,58 +11,75 @@ if (@ARGV < 1) {
 	die "usage: $0 config.yml < input.txt > output.png";
 }
 
-my %settings = %{LoadFile($ARGV[0])};
+# Load the settings.
+my %s = %{LoadFile($ARGV[0])};
 
-my ($width, $height, $block, $top, $left) = map { $settings{$_} } qw(width height block top left);
+# Create a new image, the colours, and brush we'll be using.
+my $img = GD::Image->new($s{width}, $s{height});
 
-my $img = GD::Image->new($width, $height);
-
-my $black = $img->colorAllocate(0, 0, 0);
-my $white = $img->colorAllocate(255, 255, 255);
+my %color;
+$color{black} = $img->colorAllocate(0, 0, 0);
+$color{white} = $img->colorAllocate(255, 255, 255);
 
 $img->setThickness(1);
 
 my $diagonal_brush = new GD::Image(2, 2);
-$diagonal_brush->transparent($white);
-$diagonal_brush->rectangle(0, 2, 0, 2, $black);
+$diagonal_brush->transparent($color{white});
+$diagonal_brush->rectangle(0, 2, 0, 2, $color{black});
 $img->setBrush($diagonal_brush);
 
-sub stripe {
+# Generate stripes over the given rectangle.
+sub stripify {
 	my ($img, $x1, $y1, $x2, $y2, $rev) = @_;
 
 	my ($width, $height) = ($x2 - $x1, $y2 - $y1);
 	my ($down, $right, $dist);
+	my ($x_start, $x_end, $y_start, $y_end);
 
-	($x1, $y1, $x2, $y2) = map { $_ - 1 } ($x1, $y1, $x2, $y2);
-
-	$img->filledRectangle($x1, $y1, $x2, $y2, $white);
-
-	$rev = defined $rev && $rev;
+	$img->filledRectangle($x1, $y1, $x2, $y2, $color{white});
 
 	foreach (0 .. $width / 7) {
 		$right = $width - $_ * 7;
 		$dist = $right > $height ? $height : $right;
 
-		$img->line($x1 + $_ * 7, ($rev ? $y1 : $y2), $x1 + $_ * 7 + $dist, ($rev ? $y1 + $dist: $y2 - $dist), gdBrushed);
+		$x_start = $x1 + $_ * 7;
+		$y_start = $rev ? $y1 : $y2;
+		$x_end = $x_start + $dist;
+		$y_end = $y_start + $dist * ($rev ? 1 : -1);
+
+		$img->line($x_start, $y_start, $x_end, $y_end, gdBrushed);
 	}
 
 	foreach (0 .. $height / 7) {
 		$down = $height - $_ * 7;
 		$dist = $width > $down ? $down : $width;
 
-		$img->line($x1, ($rev ? $y1 + $_ * 7 : $y2 - $_ * 7), $x1 + $dist, ($rev ? $y1 + $dist + $_ * 7 : $y2 - $dist - $_ * 7), gdBrushed);
+		$x_start = $x1;
+		$y_start = $rev ? $y1 + $_ * 7 : $y2 - $_ * 7;
+		$x_end = $x_start + $dist;
+		$y_end = $y_start + $dist * ($rev ? 1 : -1);
+
+		$img->line($x_start, $y_start, $x_end, $y_end, gdBrushed);
 	}
 }
 
-stripe($img, 1, 1, $width, $height, 1);
+# Draw reverse stripes over the entire image.
+stripify($img, 0, 0, $s{width} - 1, $s{height} - 1, 1);
 
+# Map each input character to an output block.
 my ($x, $y) = (0, 0);
 foreach (<STDIN>) {
 	chomp;
 
 	foreach (split //) {
 		if ($_ ne ' ') {
-			stripe($img, $block * ($left + $x) + 1, $block * ($top + $y) + 1, $block * ($left + $x + 1), $block * ($top + $y + 1));
+			my $x1 = $s{block} * ($s{left} + $x);
+			my $y1 = $s{block} * ($s{top} + $y);
+			my $x2 = $s{block} * ($s{left} + $x + 1) - 1;
+			my $y2 = $s{block} * ($s{top} + $y + 1) - 1;
+
+			# Draw forward stripes over the block.
+			stripify($img, $x1, $y1, $x2, $y2, 0);
 		}
 
 		++$x;
@@ -71,4 +88,5 @@ foreach (<STDIN>) {
 	++$y, $x = 0;
 }
 
+# Attack the eyes.
 print $img->png;
